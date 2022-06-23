e6fc1f2b6b22ac5ab3d61581b3b877097ead0e14
Daria Galichiy <daria-galichii@yandex.ru>
2022-06-23 21:27:11 +0300

    Likes

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,4 +1,15 @@
 package ru.netology
 
 fun main() {
+    print("Введите число лайков: ")
+    val likes = readLine() ?: return
+    val lastLetter = likes[likes.length - 1]
+    val threeLastLetters = likes.substring(likes.length - 3)
+
+    if (threeLastLetters == "000")  // например, понравилось трем тысячам людей
+        print("Понравилось $likes людей")
+    else if (lastLetter == '1')
+        print("Понравилось $likes человеку")    // например, понравилось девятьсот пятидесяти одному человеку
+    else
+        print("Понравилось $likes людям")   // например, понравилось пятнадцати людям
 }
