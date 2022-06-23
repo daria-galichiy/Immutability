d0bf6076ff3a5567561a1a58353edde07234c77a
Daria Galichiy <daria-galichii@yandex.ru>
2022-06-23 20:09:31 +0300

    Money transfer

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -1,4 +1,18 @@
 package ru.netology
 
+
+const val COMMISSION_PERCENT = 0.75
+const val MIN_COMMISSION = 3500
+
 fun main() {
+    print("Введите сумму денежного перевода в копейках: ")
+    val transferAmount = readLine()?.toUInt() ?: return
+
+    val commissionAmount = transferAmount.toDouble() * COMMISSION_PERCENT / 100
+
+    val finalCommission = if (commissionAmount > MIN_COMMISSION)
+        commissionAmount.toUInt()
+    else
+        MIN_COMMISSION
+    print("При сумме перевода $transferAmount коп. комиссия составит $finalCommission коп.")
 }
